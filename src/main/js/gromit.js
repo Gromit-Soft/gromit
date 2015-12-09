4ac8f8dc8b89e9dbbd27bae8463000b2ec64b1cb
Zack Grossbart <zack@grossbart.com>
2015-12-09 13:06:57 -0500

    We are now more careful about accessing the best locale.

--- src/main/js/gromit.js
+++ src/main/js/gromit.js
@@ -46,7 +46,7 @@ var gromit = {
     addWideStyle: function() {
         if ($(window).width() >= 1400 &&
             !gromit.wideCSS) {
-            gromit.wideCSS = gromit.addCSSLink('css/uncompressed_css/client.wide.css');
+            gromit.wideCSS = gromit.addCSSLink('gromit/css/uncompressed_css/client.wide.css');
             return;
         }
 
@@ -85,7 +85,7 @@ var gromit = {
     addMediumStyle: function() {
         if (!gromit.mediumCSS &&
             $(window).width() <= 1024) {
-            gromit.mediumCSS = gromit.addCSSLink('css/uncompressed_css/client.medium.css');
+            gromit.mediumCSS = gromit.addCSSLink('gromit/css/uncompressed_css/client.medium.css');
             return;
         }
 
@@ -102,7 +102,7 @@ var gromit = {
     addNarrowStyle: function() {
         if (!gromit.narrowCSS &&
             $(window).width() < 768) {
-            gromit.narrowCSS = gromit.addCSSLink('css/uncompressed_css/client.narrow.css');
+            gromit.narrowCSS = gromit.addCSSLink('gromit/css/uncompressed_css/client.narrow.css');
             return;
         }
 
@@ -124,72 +124,46 @@ var gromit = {
      * @private
      */
     init: function() {
-        if (gromit.bestLocale.indexOf('_#') !== -1) {
-            /*
-             * This means that the browser determined one of the new compound
-             * locales.  However, the moment library doesn't support those yet.
-             * That means we need to strip off the last part.
+        if (gromit.bestLocale) {
+            if (gromit.bestLocale.indexOf('_#') !== -1) {
+                /*
+                 * This means that the browser determined one of the new compound
+                 * locales.  However, the moment library doesn't support those yet.
+                 * That means we need to strip off the last part.
+                 */
+                gromit.bestLocale = gromit.bestLocale.substring(0, gromit.bestLocale.indexOf('_#'));
+            }
+
+            moment.locale(gromit.bestLocale);
+
+            /* 
+             * We want to specify the formats for the short dates in English
+             * so they don't have the starting zero.
              */
-            gromit.bestLocale = gromit.bestLocale.substring(0, gromit.bestLocale.indexOf('_#'));
-        }
-
-        moment.locale(gromit.bestLocale);
-
-        /* 
-         * We want to specify the formats for the short dates in English
-         * so they don't have the starting zero.
-         */
-        if (gromit.bestLocale.indexOf('en') === 0) {
-            moment.locale('en', {
-                longDateFormat: {
-                    L: 'M/D/YYYY',
-                    LT: 'h:mm A',
-                    LL : 'MMMM Do YYYY',
-                    LLLL: 'ddd MMMM D YYYY LT'
-                }
-            });
+            if (gromit.bestLocale.indexOf('en') === 0) {
+                moment.locale('en', {
+                    longDateFormat: {
+                        L: 'M/D/YYYY',
+                        LT: 'h:mm A',
+                        LL : 'MMMM Do YYYY',
+                        LLLL: 'ddd MMMM D YYYY LT'
+                    }
+                });
+            }
         }
 
         var localeData = moment.localeData();
 
-        define('jquery', [], function() {
-            return jQuery;
-        });
-
         gromit.token = $.cookie('gromitTokenCookie');
         gromit.tokenType = $.cookie('gromitTokenTypeCookie');
 
-        /*
-         * Add css lib
-         */
-        gromit.addCSSLink('css/lib/bootstrap.min.css');
-        gromit.addCSSLink('css/lib/ng-tags-input.min.css');
-
         if (gromit.debugMode) {
-            gromit.addCSSLink('css/humanMsg.css');
-            gromit.addCSSLink('css/reset.css');
-            gromit.addCSSLink('css/coreui.css');
-            gromit.addCSSLink('css/client.css');
-            gromit.addCSSLink('css/tableWidget.css');
-            gromit.addCSSLink('css/pageControls.css');
-            gromit.addCSSLink('css/review.css');
-            gromit.addCSSLink('css/reviewList.css');
-            gromit.addCSSLink('css/nav.css');
-            gromit.addCSSLink('css/overview.css');
-            gromit.addCSSLink('css/collection.css');
-            gromit.addCSSLink('css/catalog.css');
-            gromit.addCSSLink('css/header.css');
-            gromit.addCSSLink('css/markdown.css');
-            gromit.addCSSLink('css/riskCoverage.css');
-            gromit.addCSSLink('css/settings.css');
-            gromit.addCSSLink('css/group.css');
-            gromit.addCSSLink('css/perm.css');
-            gromit.addCSSLink('css/vis.css');
-            gromit.addCSSLink('css/rolePolicy.css');
-            gromit.addCSSLink('css/fulfillment.css');
-            gromit.addCSSLink('css/advisorFeeds.css');
+            gromit.addCSSLink('gromit/css/humanMsg.css');
+            gromit.addCSSLink('gromit/css/reset.css');
+            gromit.addCSSLink('gromit/css/coreui.css');
+            gromit.addCSSLink('gromit/css/client.css');
         } else {
-            gromit.addCSSLink('css/gromit-all-min.css');
+            gromit.addCSSLink('gromit/css/gromit-all-min.css');
         }
 
         gromit.isiPad = navigator.userAgent.match(/iPad/i) !== null;
@@ -201,17 +175,17 @@ var gromit = {
          */
         var browserType = navigator.userAgent.toLowerCase();
         if (browserType.indexOf('msie') > -1 || browserType.match(/trident.+rv:11./)) {
-            gromit.addCSSLink('css/uncompressed_css/msie.css');
+            gromit.addCSSLink('gromit/css/uncompressed_css/msie.css');
         } else if (browserType.indexOf('chrome') > -1) {
-            gromit.addCSSLink('css/uncompressed_css/webkit.css');
+            gromit.addCSSLink('gromit/css/uncompressed_css/webkit.css');
         } else if (browserType.indexOf('safari') > -1) {
-            gromit.addCSSLink('css/uncompressed_css/safari.css');
+            gromit.addCSSLink('gromit/css/uncompressed_css/safari.css');
         } else if (browserType.indexOf('firefox') > -1) {
-            gromit.addCSSLink('css/uncompressed_css/firefox.css');
+            gromit.addCSSLink('gromit/css/uncompressed_css/firefox.css');
         } 
 
         if (gromit.isiPad) {
-            gromit.addCSSLink('css/uncompressed_css/ipad.css');
+            gromit.addCSSLink('gromit/css/uncompressed_css/ipad.css');
         }
 
         gromit.addCSSLink('custom/custom.css');
